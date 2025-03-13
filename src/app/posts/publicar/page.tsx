6667ef4a87808e8fd60fa159adb96d5bcfcd4ed5
caKo <cakoonrails@gmail.com>
2025-03-13 15:26:59 -0300

    adding create post behavior

--- src/app/posts/publicar/page.tsx
+++ src/app/posts/publicar/page.tsx
@@ -1,4 +1,34 @@
+"use client";
+import { useState } from "react";
+
 export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [source, setSource] = useState("");
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3333/post/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          content,
+        }),
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao criar o post");
+      }
+
+      const data = await response.json();
+    } catch (error) {}
+  };
+
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center justify-center py-20 p-6 w-full max-w-7xl">
@@ -6,16 +36,19 @@ export default function CreatePost() {
           <div className="flex justify-start w-full max-w-6xl mr-2">
             <h1 className="text-3xl font-bold text-gray-900">Publicar novo conteúdo</h1>
           </div>
-          <form className="w-full bg-white p-8 rounded-lg py-2">
-            <label htmlFor="content" className="block font-semibold text-xl font-medium text-gray-800 ml-1">
+          <form className="w-full bg-white p-8 rounded-lg py-2" onSubmit={handleSubmit}>
+            <label htmlFor="title" className="block font-semibold text-xl font-medium text-gray-800 ml-1">
               Título*
             </label>
             <textarea
               id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
               placeholder="pense num título legal"
               className="w-full p-2 px-5 h-10 bg-gray-100 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500 mb-6"
               required
             ></textarea>
+
             <div className="space-y-6">
               <div>
                 <label htmlFor="content" className="block font-semibold text-xl font-medium text-gray-800 ml-1">
@@ -23,6 +56,8 @@ export default function CreatePost() {
                 </label>
                 <textarea
                   id="content"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
                   className="w-full p-6 h-100 bg-gray-100 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500"
                   required
                 ></textarea>
@@ -34,6 +69,8 @@ export default function CreatePost() {
                 </label>
                 <textarea
                   id="source"
+                  value={source}
+                  onChange={(e) => setSource(e.target.value)}
                   placeholder="fonte@dahora.com"
                   className="w-full p-2 px-5 h-10 bg-gray-100 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500"
                   required
@@ -45,11 +82,15 @@ export default function CreatePost() {
               </div>
 
               <div className="flex justify-end space-x-6 mt-6">
-                <button className="px-8 py-3 font-bold text-gray-700 rounded-md  focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all">
+                <button
+                  type="button"
+                  className="px-8 py-3 font-bold text-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all"
+                >
                   Cancelar
                 </button>
                 <button
                   type="submit"
+                  onClick={handleSubmit}
                   className="px-8 py-3 font-bold bg-green-600 text-white rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 transition-all"
                 >
                   Publicar
